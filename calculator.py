d1fdfe9ffa6cdf5e16a4b4c2037e9be58b60d122
Gideon Tettehfio <gideonlambride@gmail.com>
2024-11-13 14:55:52 +0000

    divide and multiply functions added

--- calculator.py
+++ calculator.py
@@ -4,3 +4,14 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+
+def divide(first_term, second_term):
+    try:
+        return first_term / second_term
+    except ZeroDivisionError:
+        return f"cannot divide with 0"
--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,10 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 8 == calculator.multiply(4, 2)
+
+    def test_division(self):
+        assert 5 == calculator.divide(10, 2)
+        assert "cannot divide with 0" == calculator.divide(5, 0)
